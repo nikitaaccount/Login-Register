a7003eef9e56c72457aa8e358bfbe745ec7e3e72
nikitaarora <nikitaarora0709@gmail.com>
2021-08-24 19:02:23 +0530

    Project Added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors =require('cors');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 
 const encoder=bodyParser.urlencoded({extended:true});
 app.use(encoder);
+app.use(cors());
 
 app.use(session({
     name: 'session',
@@ -31,6 +33,15 @@ app.use('/', dashboardRouter);
 app.use('/', logoutRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res)=>
+{
+    res.sendFile("./public/index.html",options,function(err){
+       if(err){
+           console.log(err);
+       }
+    })
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server is running on port 5000'));
